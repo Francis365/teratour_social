d0ffd791710fc9b6a3f4a62cd381f4431902be1e
francis <Francis365@outlook.com>
2024-08-30 12:50:04 +0100

    fixes

--- android/app/src/main/kotlin/com/itera/teratour/MainActivity.kt
+++ android/app/src/main/kotlin/com/itera/teratour/MainActivity.kt
@@ -1,10 +1,10 @@
-import android.os.Bundle
+//import android.os.Bundle
 import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+//import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+    //override fun onCreate(savedInstanceState: Bundle?) {
+    //    super.onCreate(savedInstanceState)
+    //    GeneratedPluginRegistrant.registerWith(this)
+    //}
 }
