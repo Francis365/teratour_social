578eac9a1169cee64f64756b94bebfdfb9af5842
francis <Francis365@outlook.com>
2024-08-30 12:10:50 +0100

    updated MainActivity

--- android/app/src/main/kotlin/com/itera/teratour/MainActivity.kt
+++ android/app/src/main/kotlin/com/itera/teratour/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.itera.teratour
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+}
